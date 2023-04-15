16fc6ac66aa2f11503c5e2644b24bc57eb810224
Kemal Eren Sunar <kerensunar@outlook.com>
2023-04-15 23:16:20 +0300

    Add error handling to create_user endpoint and update function comments.

--- src/api/app.py
+++ src/api/app.py
@@ -13,24 +13,34 @@ db = get_db()
 
 @app.route('/create_user', methods=['POST'])
 def create_user():
-        try:
-            data = json.loads(request.data)
-            telegram_id = data['telegram_id']
-            first_name = data['first_name']
-            last_name = data['last_name']
-            chat_id = data['chat_id']
-            user_ref = db.collection('users').document(telegram_id)
-            user_ref.set({
-                'telegram_id': telegram_id,
-                'first_name': first_name,
-                'last_name': last_name,
-                'score': 0,
-                'remaining_attempts': 5,
-                'chat_id': chat_id,
-                'answered_questions': []
-            })
-            
-            return {'message': 'User created successfully!'}
-        except Exception as e:
-            print(e)
-            return jsonify({'message': 'Error occurred'})
+    try:
+        data = json.loads(request.data)
+        telegram_id = data['telegram_id']
+        first_name = data['first_name']
+        last_name = data['last_name']
+        chat_id = data['chat_id']
+        
+        # Check if user already exists in the database
+        user_ref = db.collection('users').document(telegram_id)
+        if user_ref.get().exists:
+            return {'message': 'User already exists!'}
+        
+        # Add new user to the database
+        user_ref.set({
+            'telegram_id': telegram_id,
+            'first_name': first_name,
+            'last_name': last_name,
+            'score': 0,
+            'remaining_attempts': 5,
+            'chat_id': chat_id,
+            'answered_questions': []
+        })
+        
+        return {'message': 'User created successfully!'}
+    
+    except ValueError:
+        return {'message': 'Invalid JSON data!'}
+    
+    except Exception as e:
+        print(e)
+        return {'message': 'Error occurred'}
